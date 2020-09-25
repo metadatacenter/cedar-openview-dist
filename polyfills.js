a2e893367b93eae25081a35f36daee14ed99c1b3
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-09-24 21:50:04 -0700

    node-modules are needed for dist

--- polyfills.js
+++ polyfills.js
@@ -3457,7 +3457,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /private/tmp/cedar/cedar-openview/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/egyedia/development/CEDAR/cedar-openview/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
