37cc18b54fca15a3836fa64528647a6ed8a8ad26
Attila L. Egyedi <attila.egyedi@gmail.com>
2021-01-07 15:05:46 -0800

    New version

--- polyfills.js
+++ polyfills.js
@@ -3457,7 +3457,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/egyedia/development/git_repos/CEDAR_RELEASE/cedar-openview/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/egyedia/development/CEDAR/cedar-openview/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
